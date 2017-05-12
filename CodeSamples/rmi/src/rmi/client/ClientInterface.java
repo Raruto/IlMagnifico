750cf808eca5efe14794e3e030e7f6320d263497
Raruto <Raruto@users.noreply.github.com>
2017-05-12 20:58:22 +0200

    aggiunto un esempio utile con RMI

---
+++ CodeSamples/rmi/src/rmi/client/ClientInterface.java
@@ -0,0 +1,10 @@
+package rmi.client;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ClientInterface extends Remote {
+	
+	public void notify(String object) throws RemoteException;
+	
+}
