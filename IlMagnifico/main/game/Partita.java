cc49ab50c38c27d71b26109d1197dc3098e2e2ca
Raruto <Raruto@users.noreply.github.com>
2017-05-13 20:11:16 +0200

    created "IlMagnifico" Project

---
+++ IlMagnifico/main/game/Partita.java
@@ -0,0 +1,56 @@
+package main.game;
+
+import java.util.*;
+
+import main.game.board.Tabellone;
+import main.game.board.vatican.Vaticano;
+import main.game.cards.Mazzo;
+import main.game.handlers.GestorePartita;
+import main.game.players.Giocatore;
+import main.game.res.Dado;
+
+/**
+ * 
+ */
+public class Partita {
+
+	/**
+	 * Default constructor
+	 */
+	public Partita() {
+	}
+
+	private ArrayList<Giocatore> giocatori;
+	private Tabellone tabellone;
+	private Mazzo mazzo;
+	private Dado dado;
+	private GestorePartita gestorePartita;
+	private Vaticano vaticano;
+	private int periodo;
+	private ArrayList<Giocatore> ordineTurno;
+
+	/**
+	 * @return
+	 */
+	public void run() {
+		// TODO implement here
+		return;
+	}
+
+	/**
+	 * @return
+	 */
+	public void preparaPartita() {
+		// TODO implement here
+		return;
+	}
+
+	/**
+	 * @return
+	 */
+	public void finePartita() {
+		// TODO implement here
+		return;
+	}
+
+}
