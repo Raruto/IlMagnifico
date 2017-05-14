33e5ffee639bf40194eecbbef5e1c35fadd788c4
Raruto <Matteo@ASUS-F550L>
2017-05-14 11:55:26 +0200

    Code indentation

--- IlMagnifico/main/rmi/client/Client.java
+++ IlMagnifico/main/rmi/client/Client.java
@@ -8,6 +8,8 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.Scanner;
 
 import main.rmi.server.ServerInterface;
+import rmi.client.ClientImplementation;
+import rmi.client.ClientInterface;
 
 public class Client {
 
@@ -15,13 +17,26 @@ public class Client {
 		ServerInterface server;
 		try {
 			
-			server = (ServerInterface)Naming.lookup("//localhost/Server");	//Ottengo il riferimento remoto associato alla stringa passata (contiene l'host target e l'identificativo dell'oggetto sull'host).	
-			ClientImplementation client = new ClientImplementation();	//Creo l'oggetto client normalmente.
-			ClientInterface remoteRef = (ClientInterface) UnicastRemoteObject.exportObject(client, 0);	 //Tuttavia, dato che ClientImplementation non estende la classe UnicastRemoteObject, devo creare un riferimento remoto
-																										 //all'oggetto col metodo UnicastRemoteObject.exportObject che prende come parametri l'oggetto da esportare e la porta da
-																										 //utilizzare per la connessione. Con 0 la porta viene scelta automaticamente.
-																										 //Altrimenti avrebbe tentato di serializzare l'oggetto e di passarlo come copia al server.
-																										 //In questo caso non devo associare un identificativo all'oggetto (in quanto il riferimento remoto verrà  passato al server).
+			// Ottengo il riferimento remoto associato alla stringa passata 
+			// (contiene l'host target e l'identificativo dell'oggetto sull'host).
+			server = (ServerInterface)Naming.lookup("//localhost/Server");		
+			
+			// Creo l'oggetto client normalmente.
+			ClientImplementation client = new ClientImplementation();
+			
+			// Tuttavia, dato che ClientImplementation non estende la classe 
+			// UnicastRemoteObject, devo creare un riferimento remoto
+			// all'oggetto col metodo UnicastRemoteObject.exportObject che 
+			// prende come parametri l'oggetto da esportare e la porta da
+			// utilizzare per la connessione. Con 0 la porta viene scelta 
+			// automaticamente.
+			// Altrimenti avrebbe tentato di serializzare l'oggetto e di 
+			// passarlo come copia al server.
+			// In questo caso non devo associare un identificativo 
+			// all'oggetto (in quanto il riferimento remoto verrà  passato 
+			// al server).			
+			ClientInterface remoteRef = (ClientInterface) UnicastRemoteObject.exportObject(client, 0);	 
+
 			server.addClient(remoteRef);
 			Scanner scanner = new Scanner(System.in);
 			boolean active = true;
--- IlMagnifico/main/rmi/server/Server.java
+++ IlMagnifico/main/rmi/server/Server.java
@@ -5,24 +5,37 @@ import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 
+import rmi.server.ServerImplementation;
+
 public class Server {
 	
 	public static void main(String[] args) {
 		try {
-			LocateRegistry.createRegistry(1099); //Creo un registy sulla porta 1099 (quella di default).
+			
+			// Creo un registry sulla porta 1099 (quella di default).
+			LocateRegistry.createRegistry(1099);
+		
 		} catch (RemoteException e) {
 			System.out.println("Registry già  presente!");			
 		}		
 		
 		try {
-			ServerImplementation serverImplementation = new ServerImplementation();//Creo l'oggetto da esportare normalmente (in quanto la classe ServerImplementation estende UnicastRemoteObject)			
-			Naming.rebind("Server", serverImplementation);//Aggiungo al registry l'associazione dell'oggetto serverImplementation con "//localhost/Server".																	  
+			
+			// Creo l'oggetto da esportare normalmente 
+			// (in quanto la classe ServerImplementation estende UnicastRemoteObject)
+			ServerImplementation serverImplementation = new ServerImplementation();
+			
+			// Aggiungo al registry l'associazione dell'oggetto serverImplementation
+			// con "//localhost/Server".
+			Naming.rebind("Server", serverImplementation);
+			
 		} catch (MalformedURLException e) {
 			System.err.println("Impossibile registrare l'oggetto indicato!");
 		} catch (RemoteException e) {
 			System.err.println("Errore di connessione: " + e.getMessage() + "!");
 		}		
-		//TODO: Aggiungere la possibilità  di terminare la connessione da parte del client inviando il messaggio "STOP".		
+		
+		//TODO: Aggiungere la possibilità  di terminare la connessione da parte 
+		//      del client inviando il messaggio "STOP".		
 	}
-
 }
