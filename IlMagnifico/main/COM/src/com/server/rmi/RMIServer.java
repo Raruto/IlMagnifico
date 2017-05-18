4c3f2c28c927040d623ed9995d136f94eab83453
Raruto <Matteo@ASUS-F550L>
2017-05-18 23:37:03 +0200

    Added some support methods (RMI Server)

--- IlMagnifico/main/COM/src/com/server/rmi/RMIServer.java
+++ IlMagnifico/main/COM/src/com/server/rmi/RMIServer.java
@@ -8,34 +8,49 @@ import java.rmi.registry.LocateRegistry;
 import com.server.Server;
 
 public class RMIServer {
+	private RMIServerImplementation serverImplementation;
+	private int RMI_PORT;
 
 	public RMIServer(Server server) {
-		// TODO Auto-generated constructor stub
+
+		// Creo l'oggetto da esportare normalmente
+		// (in quanto la classe ServerImplementation estende
+		// UnicastRemoteObject)
+		try {
+			serverImplementation = new RMIServerImplementation();
+		} catch (RemoteException e) {
+			System.err.println("Errore di connessione: " + e.getMessage() + "!");
+		}
 	}
 
 	public void startServer(int rmiPort) {
+		RMI_PORT=rmiPort;
 		
+		initializeRegistry();
+		publishRemoteObject();
+
+		// TODO: Aggiungere la possibilità  di terminare la connessione da parte
+		// del client inviando il messaggio "STOP".
+	}
+
+	private void initializeRegistry() {
 		try {
-			
 			// Creo un registry sulla porta "rmiPort".
-			LocateRegistry.createRegistry(rmiPort);
-			
+			LocateRegistry.createRegistry(RMI_PORT);
+
 		} catch (RemoteException e) {
 			System.out.println("Registry già  presente!");
 		}
 
+	}
+
+	private void publishRemoteObject() {
 		try {
-
-			// Creo l'oggetto da esportare normalmente
-			// (in quanto la classe ServerImplementation estende
-			// UnicastRemoteObject)
-			RMIServerImplementation serverImplementation = new RMIServerImplementation();
-
 			// Aggiungo al registry l'associazione dell'oggetto
 			// serverImplementation
 			// con "//localhost/Server".
 			Naming.rebind("Server", serverImplementation);
-			
+
 			System.out.println("[RMI Server] OK");
 
 		} catch (MalformedURLException e) {
@@ -44,8 +59,5 @@ public class RMIServer {
 			System.err.println("Errore di connessione: " + e.getMessage() + "!");
 		}
 
-		// TODO: Aggiungere la possibilità  di terminare la connessione da parte
-		// del client inviando il messaggio "STOP".
 	}
-
 }
--- IlMagnifico/main/COM/src/com/server/rmi/RMIServerImplementation.java
+++ IlMagnifico/main/COM/src/com/server/rmi/RMIServerImplementation.java
@@ -8,33 +8,32 @@ import java.util.Iterator;
 
 import com.client.rmi.ClientInterface;
 
-public class RMIServerImplementation extends UnicastRemoteObject implements
-		RMIServerInterface {
+public class RMIServerImplementation extends UnicastRemoteObject implements RMIServerInterface {
+
+	private static final long serialVersionUID = -7098548671967083832L;
 	
 	private ArrayList<ClientInterface> clients = new ArrayList<ClientInterface>();
 
 	protected RMIServerImplementation() throws RemoteException {
-		super(0);		
+		super(0);
 	}
-
-	private static final long serialVersionUID = -7098548671967083832L;
 
 	@Override
 	public void addClient(ClientInterface client) throws RemoteException {
-		clients.add(client);		
+		clients.add(client);
 	}
 
 	@Override
 	public void send(String message) throws RemoteException {
 		Iterator<ClientInterface> clientIterator = clients.iterator();
-		while(clientIterator.hasNext()){
-			try{
+		while (clientIterator.hasNext()) {
+			try {
 				clientIterator.next().notify(message);
-			}catch(ConnectException e) {
-				clientIterator.remove();	
+			} catch (ConnectException e) {
+				clientIterator.remove();
 				System.out.println("Client rimosso!");
-			}			
-		}	
+			}
+		}
 	}
 
 }
