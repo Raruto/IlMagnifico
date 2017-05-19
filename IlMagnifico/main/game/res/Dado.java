a7aaa4e12594c3ae7d51fae076c324d19ede21f9
Raruto <Matteo@ASUS-F550L>
2017-05-20 00:51:48 +0200

    Added "generaNumeroRandom()"

--- IlMagnifico/main/game/res/Dado.java
+++ IlMagnifico/main/game/res/Dado.java
@@ -10,25 +10,32 @@ public class Dado {
 	private int dadoBianco;
 	private int dadoArancione;
 	private int dadoNero;
-	
-    /**
-     * Default constructor
-     */
-    public Dado() {
-    	this.dadoBianco=0;
-    	this.dadoArancione=0;
-    	this.dadoNero=0;
-    }
 
-    /**
-     * 
-     */
-    public void lancia(){
-    	dadoBianco=new Random().nextInt(6-1)+1;
-    	dadoArancione=new Random().nextInt(6-1)+1;
-    	dadoNero=new Random().nextInt(6-1)+1;
-    };
-    
+	/**
+	 * Default constructor
+	 */
+	public Dado() {
+		this.dadoBianco = 0;
+		this.dadoArancione = 0;
+		this.dadoNero = 0;
+	}
+
+	/**
+	 * Lancia tutti i dadi
+	 */
+	public void lancia() {
+		dadoBianco = generaNumeroRandom(6);
+		dadoArancione = generaNumeroRandom(6);
+		dadoNero = generaNumeroRandom(6);
+	}
+
+	/**
+	 * @param numeroFacce
+	 */
+	private int generaNumeroRandom(int numeroFacce) {
+		return new Random().nextInt(numeroFacce - 1) + 1;
+	}
+
 	public int getDadoBianco() {
 		return dadoBianco;
 	}
@@ -40,7 +47,4 @@ public class Dado {
 	public int getDadoNero() {
 		return dadoNero;
 	}
-
-    
-
 }
