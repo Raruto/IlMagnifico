c053c7acd0880b774bcc35adb27635c0c1e7a7bf
Raruto <Matteo@ASUS-F550L>
2017-05-20 00:55:35 +0200

    Added getter methods

--- IlMagnifico/main/game/res/PuntiGiocatore.java
+++ IlMagnifico/main/game/res/PuntiGiocatore.java
@@ -7,28 +7,40 @@ import java.util.*;
  */
 public class PuntiGiocatore {
 
-    /**
-     * Default constructor
-     */
-    public PuntiGiocatore(int PV,int PM,int PF) {
-    	this.puntiVittoria=PV;
-    	this.puntiMilitari=PM;
-    	this.puntiFede=PF;
-    }
+	/**
+	 * 
+	 */
+	private int puntiVittoria;
 
-    /**
-     * 
-     */
-    private int puntiVittoria;
+	/**
+	 * 
+	 */
+	private int puntiMilitari;
 
-    /**
-     * 
-     */
-    private int puntiMilitari;
+	/**
+	 * 
+	 */
+	private int puntiFede;
 
-    /**
-     * 
-     */
-    private int puntiFede;
+	/**
+	 * Default constructor
+	 */
+	public PuntiGiocatore(int puntiVittoria, int puntiMilitari, int puntiFede) {
+		this.puntiVittoria = puntiVittoria;
+		this.puntiMilitari = puntiMilitari;
+		this.puntiFede = puntiFede;
+	}
+
+	public int getPuntiVittoria() {
+		return puntiVittoria;
+	}
+
+	public int getPuntiMilitari() {
+		return puntiMilitari;
+	}
+
+	public int getPuntiFede() {
+		return puntiFede;
+	}
 
 }
