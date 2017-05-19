f1d952ad0e90a56db8ebf5804392c2ce36f1f421
Raruto <Matteo@ASUS-F550L>
2017-05-20 00:56:41 +0200

    Added getter methods

--- IlMagnifico/main/game/res/RisorseGiocatore.java
+++ IlMagnifico/main/game/res/RisorseGiocatore.java
@@ -4,15 +4,33 @@ package main.game.res;
  * 
  */
 public class RisorseGiocatore {
-    private int legno;
-    private int denaro;
-    private int pietre;
-    private int servitori;
-    
-    public RisorseGiocatore(int legno,int denaro,int pietre,int servitori){
-    	this.legno=legno;
-    	this.denaro=denaro;
-    	this.pietre=pietre;
-    	this.servitori=servitori;
-    }
+	private int legno;
+	private int denaro;
+	private int pietre;
+	private int servitori;
+
+	public RisorseGiocatore(int legno, int denaro, int pietre, int servitori) {
+		this.legno = legno;
+		this.denaro = denaro;
+		this.pietre = pietre;
+		this.servitori = servitori;
+	}
+
+	public int getLegno() {
+		return legno;
+	}
+
+	public int getDenaro() {
+		return denaro;
+	}
+
+	public int getPietre() {
+		return pietre;
+	}
+
+	public int getServitori() {
+		return servitori;
+	}
+	
+	
 }
