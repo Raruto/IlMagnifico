bcbddadfed60a55bb1031d814c97687bd984c5cb
Raruto <Matteo@ASUS-F550L>
2017-05-20 01:46:38 +0200

    Added some JavaDoc, added "inizializzaMazzi()"

--- IlMagnifico/main/game/cards/Mazzo.java
+++ IlMagnifico/main/game/cards/Mazzo.java
@@ -2,180 +2,195 @@ package main.game.cards;
 
 import java.util.*;
 
-import main.game.cards.development.CartaEdificio;
-import main.game.cards.development.CartaImpresa;
-import main.game.cards.development.CartaPersonaggio;
-import main.game.cards.development.CartaTerritorio;
-import main.game.cards.development.CarteEdificio;
-import main.game.cards.development.CarteImpresa;
-import main.game.cards.development.CartePersonaggio;
-import main.game.cards.development.CarteTerritorio;
-
+import main.game.cards.development.*;
 
 /**
  * 
  */
 public class Mazzo {
 
-    /**
-     * Default constructor
-     */
-    public Mazzo() {
-    }
+	/**
+	 * Mazzo delle Carte Edificio associate al periodo corrente
+	 */
+	private ArrayList<CartaEdificio> carteEdificio;
 
-    /**
-     * 
-     */
-    private ArrayList<CartaTerritorio> carteTerritorio;
+	/**
+	 * Mazzo delle Carte Impresa associate al periodo corrente
+	 */
+	private ArrayList<CartaImpresa> carteImpresa;
 
-    /**
-     * 
-     */
-    private ArrayList<CartaPersonaggio> cartePersonaggio;
+	/**
+	 * Mazzo delle Carte Territorio associate al periodo corrente
+	 */
+	private ArrayList<CartaTerritorio> carteTerritorio;
 
-    /**
-     * 
-     */
-    private ArrayList<CartaEdificio> carteEdificio;
+	/**
+	 * Mazzo delle Carte Personaggio associate al periodo corrente
+	 */
+	private ArrayList<CartaPersonaggio> cartePersonaggio;
 
-    /**
-     * 
-     */
-    private ArrayList<CartaImpresa> carteImpresa;
-    
-    /**
-     * *
-     * */
-    private int periodo;
+	/**
+	 * Periodo corrente di gioco (1,2,3) (ad ogni incremento del periodo di
+	 * gioco le liste dei mazzi vengono agggiornati con le carte del realtivo
+	 * periodo)
+	 */
+	private int periodo;
 
+	/**
+	 * Default constructor
+	 */
+	public Mazzo() {
+		this.periodo = 1;
 
+		carteEdificio = new ArrayList<CartaEdificio>();
+		carteImpresa = new ArrayList<CartaImpresa>();
+		carteTerritorio = new ArrayList<CartaTerritorio>();
+		cartePersonaggio = new ArrayList<CartaPersonaggio>();
 
-    /**
-     * @return
-     */
-    public void mescolaMazzo() {
-        // Riempie gli ArrayList con le carte dei periodi corrispondenti e 
-    	// mischia gli elementi all'interno degli ArrayList
-    	// inizializzo delle variabili che mi serviranno per il mescolamento delle carte
-    	CartaTerritorio cartaTerritorioTemp1=new CartaTerritorio(null);
-    	CartaTerritorio cartaTerritorioTemp2=new CartaTerritorio(null);
-    	CartaPersonaggio cartaPersonaggioTemp1=new CartaPersonaggio(null);
-    	CartaPersonaggio cartaPersonaggioTemp2=new CartaPersonaggio(null);
-    	CartaEdificio cartaEdificioTemp1=new CartaEdificio(null);
-    	CartaEdificio cartaEdificioTemp2=new CartaEdificio(null);
-    	CartaImpresa cartaImpresaTemp1=new CartaImpresa(null);
-    	CartaImpresa cartaImpresaTemp2=new CartaImpresa(null);
-    	int index1;
-    	int index2;
-    	int contatore;
-    	Random random=new Random();
-    	
+		inizializzaMazzi();
+	}
 
-    	//scorro l'enum in cerca delle carte che mi servono, del tipo e del periodo corrispondente
-    	for(CarteTerritorio c : CarteTerritorio.values()){
-    		if(c.getPeriodoCarta()==this.periodo)
-    			this.carteTerritorio.add(0, new CartaTerritorio(c));
-    	}
-    	
-    	for(CartePersonaggio c : CartePersonaggio.values()){
-    		if(c.getPeriodoCarta()==this.periodo)
-    			this.cartePersonaggio.add(0, new CartaPersonaggio(c));
-    	}
-    	for(CarteEdificio c : CarteEdificio.values()){
-    		if(c.getPeriodoCarta()==this.periodo)
-    			this.carteEdificio.add(0, new CartaEdificio(c));
-    	}
-    	for(CarteImpresa c : CarteImpresa.values()){
-    		if(c.getPeriodoCarta()==this.periodo)
-    			this.carteImpresa.add(0, new CartaImpresa(c));
-    	}
-    		
-    	
-    	//mischio i mazzetti dei tipi di carte prendendo sempre due oggetti dell'ArrayList e scambiandoli
-    	for(contatore=0;contatore<10;contatore++){
-    		index1=random.nextInt(9);
-    		index2=random.nextInt(9);
-    		cartaPersonaggioTemp1=this.cartePersonaggio.get(index1);
-    		cartaPersonaggioTemp2=this.cartePersonaggio.get(index2);
-    		this.cartePersonaggio.set(index2, cartaPersonaggioTemp1);
-    		this.cartePersonaggio.set(index1, cartaPersonaggioTemp2);
-    	}
-    	
-    	for(contatore=0;contatore<10;contatore++){
-    		index1=random.nextInt(9);
-    		index2=random.nextInt(9);
-    		cartaTerritorioTemp1=this.carteTerritorio.get(index1);
-    		cartaTerritorioTemp2=this.carteTerritorio.get(index2);
-    		this.carteTerritorio.set(index1, cartaTerritorioTemp2);
-    		this.carteTerritorio.set(index2, cartaTerritorioTemp1);
-    	}
-    	for(contatore=0;contatore<10;contatore++){
-    		index1=random.nextInt(9);
-    		index2=random.nextInt(9);
-    		cartaEdificioTemp1=this.carteEdificio.get(index1);
-    		cartaEdificioTemp2=this.carteEdificio.get(index2);
-    		this.carteEdificio.set(index1, cartaEdificioTemp2);
-    		this.carteEdificio.set(index2, cartaEdificioTemp1);
-    	}
-    	for(contatore=0;contatore<10;contatore++){
-    		index1=random.nextInt(9);
-    		index2=random.nextInt(9);
-    		cartaImpresaTemp1=this.carteImpresa.get(index1);
-    		cartaImpresaTemp2=this.carteImpresa.get(index2);
-    		this.carteImpresa.set(index2, cartaImpresaTemp1);
-    		this.carteImpresa.set(index1, cartaImpresaTemp2);
-    	}
-        return;
-    }
+	/**
+	 * Inizializza i mazzi sulla base del Periodo corrente
+	 * 
+	 * @return void
+	 */
+	private void inizializzaMazzi() {
+		for (CarteTerritorio c : CarteTerritorio.values()) {
+			if (c.getPeriodoCarta() == this.periodo)
+				this.carteTerritorio.add(0, new CartaTerritorio(c));
+		}
 
-    /**
-     * Esegue il POP della prima carta dalla lista delle Carte Edificio 
-     * @return CartaEdificio
-     */
-    public CartaEdificio getCartaEdificio() {
-    	CartaEdificio cartaTemporanea=new CartaEdificio(null);
-    	cartaTemporanea=this.carteEdificio.get(0);
-    	this.carteEdificio.remove(0);
-        return cartaTemporanea;
-    }
+		for (CartePersonaggio c : CartePersonaggio.values()) {
+			if (c.getPeriodoCarta() == this.periodo)
+				this.cartePersonaggio.add(0, new CartaPersonaggio(c));
+		}
+		for (CarteEdificio c : CarteEdificio.values()) {
+			if (c.getPeriodoCarta() == this.periodo)
+				this.carteEdificio.add(0, new CartaEdificio(c));
+		}
+		for (CarteImpresa c : CarteImpresa.values()) {
+			if (c.getPeriodoCarta() == this.periodo)
+				this.carteImpresa.add(0, new CartaImpresa(c));
+		}
 
-    /**
-     * Esegue il POP della prima carta dalla lista delle Carte Territorio 
-     * @return CartaTerritorio
-     */
-    public CartaTerritorio getCartaTerritorio() {
-    	CartaTerritorio cartaTemporanea=new CartaTerritorio(null);
-    	cartaTemporanea=this.carteTerritorio.get(0);
-    	this.carteTerritorio.remove(0);
-        return cartaTemporanea;
-    }
+	}
 
-    /**
-     * Esegue il POP della prima carta dalla lista delle Carte Personaggio 
-     * @return CartaPersonaggio
-     */
-    public CartaPersonaggio getCartaPersonaggio() {
-    	CartaPersonaggio cartaTemporanea=new CartaPersonaggio(null);
-    	cartaTemporanea=this.cartePersonaggio.get(0);
-    	this.cartePersonaggio.remove(0);
-        return cartaTemporanea;
-    }
+	/**
+	 * Riempie gli ArrayList con le carte dei periodi corrispondenti e mischia
+	 * gli elementi all'interno degli ArrayList inizializzo delle variabili che
+	 * mi serviranno per il mescolamento delle carte
+	 * 
+	 * @return void
+	 */
+	public void mescolaMazzo() {
+		CartaTerritorio cartaTerritorioTemp1 = new CartaTerritorio(null);
+		CartaTerritorio cartaTerritorioTemp2 = new CartaTerritorio(null);
+		CartaPersonaggio cartaPersonaggioTemp1 = new CartaPersonaggio(null);
+		CartaPersonaggio cartaPersonaggioTemp2 = new CartaPersonaggio(null);
+		CartaEdificio cartaEdificioTemp1 = new CartaEdificio(null);
+		CartaEdificio cartaEdificioTemp2 = new CartaEdificio(null);
+		CartaImpresa cartaImpresaTemp1 = new CartaImpresa(null);
+		CartaImpresa cartaImpresaTemp2 = new CartaImpresa(null);
+		int index1;
+		int index2;
+		int contatore;
+		Random random = new Random();
 
-    /**
-     * Esegue il POP della prima carta dalla lista delle Carte Impresa 
-     * @return CartaImpresa
-     */
-    public CartaImpresa getCartaImpresa() {
-    	CartaImpresa cartaTemporanea=new CartaImpresa(null);
-    	cartaTemporanea=this.carteImpresa.get(0);
-    	this.carteImpresa.remove(0);
-        return cartaTemporanea;
-    }
-    
-    public void setPeriodo(int periodoNuovo){
-    	this.periodo=periodoNuovo;
-    }
-    
+		inizializzaMazzi();
 
+		// mischio i mazzetti dei tipi di carte prendendo sempre due oggetti
+		// dell'ArrayList e scambiandoli
+		for (contatore = 0; contatore < 10; contatore++) {
+			index1 = random.nextInt(9);
+			index2 = random.nextInt(9);
+			cartaPersonaggioTemp1 = this.cartePersonaggio.get(index1);
+			cartaPersonaggioTemp2 = this.cartePersonaggio.get(index2);
+			this.cartePersonaggio.set(index2, cartaPersonaggioTemp1);
+			this.cartePersonaggio.set(index1, cartaPersonaggioTemp2);
+		}
+
+		for (contatore = 0; contatore < 10; contatore++) {
+			index1 = random.nextInt(9);
+			index2 = random.nextInt(9);
+			cartaTerritorioTemp1 = this.carteTerritorio.get(index1);
+			cartaTerritorioTemp2 = this.carteTerritorio.get(index2);
+			this.carteTerritorio.set(index1, cartaTerritorioTemp2);
+			this.carteTerritorio.set(index2, cartaTerritorioTemp1);
+		}
+		for (contatore = 0; contatore < 10; contatore++) {
+			index1 = random.nextInt(9);
+			index2 = random.nextInt(9);
+			cartaEdificioTemp1 = this.carteEdificio.get(index1);
+			cartaEdificioTemp2 = this.carteEdificio.get(index2);
+			this.carteEdificio.set(index1, cartaEdificioTemp2);
+			this.carteEdificio.set(index2, cartaEdificioTemp1);
+		}
+		for (contatore = 0; contatore < 10; contatore++) {
+			index1 = random.nextInt(9);
+			index2 = random.nextInt(9);
+			cartaImpresaTemp1 = this.carteImpresa.get(index1);
+			cartaImpresaTemp2 = this.carteImpresa.get(index2);
+			this.carteImpresa.set(index2, cartaImpresaTemp1);
+			this.carteImpresa.set(index1, cartaImpresaTemp2);
+		}
+		return;
+	}
+
+	/**
+	 * Esegue il POP della prima carta dalla lista delle Carte Edificio
+	 * 
+	 * @return CartaEdificio
+	 */
+	public CartaEdificio getCartaEdificio() {
+		CartaEdificio cartaTemporanea = new CartaEdificio(null);
+		cartaTemporanea = this.carteEdificio.get(0);
+		this.carteEdificio.remove(0);
+		return cartaTemporanea;
+	}
+
+	/**
+	 * Esegue il POP della prima carta dalla lista delle Carte Territorio
+	 * 
+	 * @return CartaTerritorio
+	 */
+	public CartaTerritorio getCartaTerritorio() {
+		CartaTerritorio cartaTemporanea = new CartaTerritorio(null);
+		cartaTemporanea = this.carteTerritorio.get(0);
+		this.carteTerritorio.remove(0);
+		return cartaTemporanea;
+	}
+
+	/**
+	 * Esegue il POP della prima carta dalla lista delle Carte Personaggio
+	 * 
+	 * @return CartaPersonaggio
+	 */
+	public CartaPersonaggio getCartaPersonaggio() {
+		CartaPersonaggio cartaTemporanea = new CartaPersonaggio(null);
+		cartaTemporanea = this.cartePersonaggio.get(0);
+		this.cartePersonaggio.remove(0);
+		return cartaTemporanea;
+	}
+
+	/**
+	 * Esegue il POP della prima carta dalla lista delle Carte Impresa
+	 * 
+	 * @return CartaImpresa
+	 */
+	public CartaImpresa getCartaImpresa() {
+		CartaImpresa cartaTemporanea = new CartaImpresa(null);
+		cartaTemporanea = this.carteImpresa.get(0);
+		this.carteImpresa.remove(0);
+		return cartaTemporanea;
+	}
+
+	/* NB questo lo toglierei al suo posto usa: incrementaPerdio()*/
+	/*
+	 * public void setPeriodo(int periodoNuovo) { this.periodo = periodoNuovo; }
+	 */
+	public void incrementaPerdio() {
+		this.periodo++;
+	}
 
 }
