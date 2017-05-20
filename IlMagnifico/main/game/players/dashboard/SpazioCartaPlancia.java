82aef352d3c08699efafa3d4f4459d413999dc7b
Raruto <Matteo@ASUS-F550L>
2017-05-20 17:12:23 +0200

    Added JavaDOC

--- IlMagnifico/main/game/players/dashboard/SpazioCartaPlancia.java
+++ IlMagnifico/main/game/players/dashboard/SpazioCartaPlancia.java
@@ -1,41 +1,39 @@
 package main.game.players.dashboard;
 
-import java.util.*;
-
 import main.game.cards.development.CartaSviluppo;
-import main.game.spaces.SpazioAzione;
 
 /**
- * 
+ * Classe contenitore per una singola Carta Sviluppo all'interno della Plancia
+ * Giocatore
  */
-public class SpazioCartaPlancia implements SpazioCartaSviluppo{
+public class SpazioCartaPlancia implements SpazioCartaSviluppo {
 
-    /**
-     * Default constructor
-     */
-    public SpazioCartaPlancia() {
-    }
+	/**
+	 * Default constructor
+	 */
+	public SpazioCartaPlancia() {
+	}
 
-    /**
-     * 
-     */
-    private CartaSviluppo carta;
+	/**
+	 * 
+	 */
+	private CartaSviluppo carta;
 
-    /**
-     * @return
-     */
-    public CartaSviluppo visualizzaCarta() {
-        // TODO implement here
-        return null;
-    }
+	/**
+	 * @return
+	 */
+	public CartaSviluppo visualizzaCarta() {
+		// TODO implement here
+		return null;
+	}
 
-    /**
-     * @param carta 
-     * @return
-     */
-    public void aggiungiCarta(CartaSviluppo carta) {
-        // TODO implement here
-        return;
-    }
+	/**
+	 * @param carta
+	 * @return
+	 */
+	public void aggiungiCarta(CartaSviluppo carta) {
+		// TODO implement here
+		return;
+	}
 
 }
--- IlMagnifico/main/game/players/dashboard/SpazioCartaSviluppo.java
+++ IlMagnifico/main/game/players/dashboard/SpazioCartaSviluppo.java
@@ -3,15 +3,13 @@ package main.game.players.dashboard;
 import main.game.cards.development.CartaSviluppo;
 
 /**
- * Classe contenitore per una singola Carta Sviluppo all'interno della Plancia
- * Giocatore
+ * Interfaccia contenitore per una singola Carta Sviluppo
  */
 public interface SpazioCartaSviluppo {
 	public CartaSviluppo visualizzaCarta();
 
 	/**
 	 * @param carta
-	 * @return
 	 */
 	public void aggiungiCarta(CartaSviluppo carta);
 
