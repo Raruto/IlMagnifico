5048fbf08e5adcad071fb2e4fc6e19c7ae5c00f1
Raruto <Matteo@ASUS-F550L>
2017-05-20 22:34:18 +0200

    Added "incrementaAttivazioni()"

--- IlMagnifico/main/game/activatables/Attivabile.java
+++ IlMagnifico/main/game/activatables/Attivabile.java
@@ -6,8 +6,14 @@ public class Attivabile {
 
 	private int contatoreAttivazioni;
 	private boolean permanente;
-	
-	public void attiva(Giocatore giocatore){
-		
+
+	public void attiva(Giocatore giocatore) {
+		//TODO: if(attivabile?){
+		incrementaAttivazioni()
+		//}
+	}
+
+	private void incrementaAttivazioni() {
+		contatoreAttivazioni++;
 	}
 }
