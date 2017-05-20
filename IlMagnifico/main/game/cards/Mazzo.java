59c51e6c80d01b64d7b3b313f7d884aff1342eb6
Raruto <Matteo@ASUS-F550L>
2017-05-20 22:28:44 +0200

    Renamed variables into "Mazzo"

--- IlMagnifico/main/game/cards/Mazzo.java
+++ IlMagnifico/main/game/cards/Mazzo.java
@@ -12,40 +12,40 @@ public class Mazzo {
 	/**
 	 * Mazzo delle Carte Edificio associate al periodo corrente
 	 */
-	private ArrayList<CartaEdificio> carteEdificio;
+	private ArrayList<CartaEdificio> mazzoCarteEdificio;
 
 	/**
 	 * Mazzo delle Carte Impresa associate al periodo corrente
 	 */
-	private ArrayList<CartaImpresa> carteImpresa;
+	private ArrayList<CartaImpresa> mazzoCarteImpresa;
 
 	/**
 	 * Mazzo delle Carte Territorio associate al periodo corrente
 	 */
-	private ArrayList<CartaTerritorio> carteTerritorio;
+	private ArrayList<CartaTerritorio> mazzoCarteTerritorio;
 
 	/**
 	 * Mazzo delle Carte Personaggio associate al periodo corrente
 	 */
-	private ArrayList<CartaPersonaggio> cartePersonaggio;
+	private ArrayList<CartaPersonaggio> mazzoCartePersonaggio;
 
 	/**
 	 * Periodo corrente di gioco (1,2,3) (ad ogni incremento del periodo di
 	 * gioco le liste dei mazzi vengono aggiornati con le carte del relativo
 	 * periodo)
 	 */
-	private int periodo;
+	private int periodoCorrente;
 
 	/**
 	 * Default constructor
 	 */
 	public Mazzo() {
-		this.periodo = 1;
+		this.periodoCorrente = 1;
 
-		carteEdificio = new ArrayList<CartaEdificio>();
-		carteImpresa = new ArrayList<CartaImpresa>();
-		carteTerritorio = new ArrayList<CartaTerritorio>();
-		cartePersonaggio = new ArrayList<CartaPersonaggio>();
+		mazzoCarteEdificio = new ArrayList<CartaEdificio>();
+		mazzoCarteImpresa = new ArrayList<CartaImpresa>();
+		mazzoCarteTerritorio = new ArrayList<CartaTerritorio>();
+		mazzoCartePersonaggio = new ArrayList<CartaPersonaggio>();
 
 		inizializzaMazzi();
 	}
@@ -57,21 +57,21 @@ public class Mazzo {
 	 */
 	private void inizializzaMazzi() {
 		for (CarteTerritorio c : CarteTerritorio.values()) {
-			if (c.getPeriodoCarta() == this.periodo)
-				this.carteTerritorio.add(0, new CartaTerritorio(c));
+			if (c.getPeriodoCarta() == this.periodoCorrente)
+				this.mazzoCarteTerritorio.add(0, new CartaTerritorio(c));
 		}
 
 		for (CartePersonaggio c : CartePersonaggio.values()) {
-			if (c.getPeriodoCarta() == this.periodo)
-				this.cartePersonaggio.add(0, new CartaPersonaggio(c));
+			if (c.getPeriodoCarta() == this.periodoCorrente)
+				this.mazzoCartePersonaggio.add(0, new CartaPersonaggio(c));
 		}
 		for (CarteEdificio c : CarteEdificio.values()) {
-			if (c.getPeriodoCarta() == this.periodo)
-				this.carteEdificio.add(0, new CartaEdificio(c));
+			if (c.getPeriodoCarta() == this.periodoCorrente)
+				this.mazzoCarteEdificio.add(0, new CartaEdificio(c));
 		}
 		for (CarteImpresa c : CarteImpresa.values()) {
-			if (c.getPeriodoCarta() == this.periodo)
-				this.carteImpresa.add(0, new CartaImpresa(c));
+			if (c.getPeriodoCarta() == this.periodoCorrente)
+				this.mazzoCarteImpresa.add(0, new CartaImpresa(c));
 		}
 
 	}
@@ -102,37 +102,37 @@ public class Mazzo {
 		// mischio i mazzetti dei tipi di carte prendendo sempre due oggetti
 		// dell'ArrayList e scambiandoli
 		for (contatore = 0; contatore < 10; contatore++) {
-			index1 = random.nextInt(this.cartePersonaggio.size());
-			index2 = random.nextInt(this.cartePersonaggio.size()); //anzichè un numero generico utilizzerei la dimensione effettiva dell'arraylist
-			cartaPersonaggioTemp1 = this.cartePersonaggio.get(index1);
-			cartaPersonaggioTemp2 = this.cartePersonaggio.get(index2);
-			this.cartePersonaggio.set(index2, cartaPersonaggioTemp1);
-			this.cartePersonaggio.set(index1, cartaPersonaggioTemp2);
+			index1 = random.nextInt(this.mazzoCartePersonaggio.size());
+			index2 = random.nextInt(this.mazzoCartePersonaggio.size()); //anzichè un numero generico utilizzerei la dimensione effettiva dell'arraylist
+			cartaPersonaggioTemp1 = this.mazzoCartePersonaggio.get(index1);
+			cartaPersonaggioTemp2 = this.mazzoCartePersonaggio.get(index2);
+			this.mazzoCartePersonaggio.set(index2, cartaPersonaggioTemp1);
+			this.mazzoCartePersonaggio.set(index1, cartaPersonaggioTemp2);
 		}
 
 		for (contatore = 0; contatore < 10; contatore++) {
-			index1 = random.nextInt(this.carteTerritorio.size());
-			index2 = random.nextInt(this.carteTerritorio.size());
-			cartaTerritorioTemp1 = this.carteTerritorio.get(index1);
-			cartaTerritorioTemp2 = this.carteTerritorio.get(index2);
-			this.carteTerritorio.set(index1, cartaTerritorioTemp2);
-			this.carteTerritorio.set(index2, cartaTerritorioTemp1);
+			index1 = random.nextInt(this.mazzoCarteTerritorio.size());
+			index2 = random.nextInt(this.mazzoCarteTerritorio.size());
+			cartaTerritorioTemp1 = this.mazzoCarteTerritorio.get(index1);
+			cartaTerritorioTemp2 = this.mazzoCarteTerritorio.get(index2);
+			this.mazzoCarteTerritorio.set(index1, cartaTerritorioTemp2);
+			this.mazzoCarteTerritorio.set(index2, cartaTerritorioTemp1);
 		}
 		for (contatore = 0; contatore < 10; contatore++) {
-			index1 = random.nextInt(this.carteEdificio.size());
-			index2 = random.nextInt(this.carteEdificio.size());
-			cartaEdificioTemp1 = this.carteEdificio.get(index1);
-			cartaEdificioTemp2 = this.carteEdificio.get(index2);
-			this.carteEdificio.set(index1, cartaEdificioTemp2);
-			this.carteEdificio.set(index2, cartaEdificioTemp1);
+			index1 = random.nextInt(this.mazzoCarteEdificio.size());
+			index2 = random.nextInt(this.mazzoCarteEdificio.size());
+			cartaEdificioTemp1 = this.mazzoCarteEdificio.get(index1);
+			cartaEdificioTemp2 = this.mazzoCarteEdificio.get(index2);
+			this.mazzoCarteEdificio.set(index1, cartaEdificioTemp2);
+			this.mazzoCarteEdificio.set(index2, cartaEdificioTemp1);
 		}
 		for (contatore = 0; contatore < 10; contatore++) {
-			index1 = random.nextInt(this.carteImpresa.size());
-			index2 = random.nextInt(this.carteImpresa.size());
-			cartaImpresaTemp1 = this.carteImpresa.get(index1);
-			cartaImpresaTemp2 = this.carteImpresa.get(index2);
-			this.carteImpresa.set(index2, cartaImpresaTemp1);
-			this.carteImpresa.set(index1, cartaImpresaTemp2);
+			index1 = random.nextInt(this.mazzoCarteImpresa.size());
+			index2 = random.nextInt(this.mazzoCarteImpresa.size());
+			cartaImpresaTemp1 = this.mazzoCarteImpresa.get(index1);
+			cartaImpresaTemp2 = this.mazzoCarteImpresa.get(index2);
+			this.mazzoCarteImpresa.set(index2, cartaImpresaTemp1);
+			this.mazzoCarteImpresa.set(index1, cartaImpresaTemp2);
 		}
 		return;
 	}
@@ -144,8 +144,8 @@ public class Mazzo {
 	 */
 	public CartaEdificio getCartaEdificio() {
 		CartaEdificio cartaTemporanea = new CartaEdificio(null);
-		cartaTemporanea = this.carteEdificio.get(0);
-		this.carteEdificio.remove(0);
+		cartaTemporanea = this.mazzoCarteEdificio.get(0);
+		this.mazzoCarteEdificio.remove(0);
 		return cartaTemporanea;
 	}
 
@@ -156,8 +156,8 @@ public class Mazzo {
 	 */
 	public CartaTerritorio getCartaTerritorio() {
 		CartaTerritorio cartaTemporanea = new CartaTerritorio(null);
-		cartaTemporanea = this.carteTerritorio.get(0);
-		this.carteTerritorio.remove(0);
+		cartaTemporanea = this.mazzoCarteTerritorio.get(0);
+		this.mazzoCarteTerritorio.remove(0);
 		return cartaTemporanea;
 	}
 
@@ -168,8 +168,8 @@ public class Mazzo {
 	 */
 	public CartaPersonaggio getCartaPersonaggio() {
 		CartaPersonaggio cartaTemporanea = new CartaPersonaggio(null);
-		cartaTemporanea = this.cartePersonaggio.get(0);
-		this.cartePersonaggio.remove(0);
+		cartaTemporanea = this.mazzoCartePersonaggio.get(0);
+		this.mazzoCartePersonaggio.remove(0);
 		return cartaTemporanea;
 	}
 
@@ -180,8 +180,8 @@ public class Mazzo {
 	 */
 	public CartaImpresa getCartaImpresa() {
 		CartaImpresa cartaTemporanea = new CartaImpresa(null);
-		cartaTemporanea = this.carteImpresa.get(0);
-		this.carteImpresa.remove(0);
+		cartaTemporanea = this.mazzoCarteImpresa.get(0);
+		this.mazzoCarteImpresa.remove(0);
 		return cartaTemporanea;
 	}
 
@@ -190,7 +190,7 @@ public class Mazzo {
 	 * Incrementa l'attributo periodo di 1
 	 */
 	public void incrementaPeriodo() {
-		this.periodo++;
+		this.periodoCorrente++;
 	}
 
 }
