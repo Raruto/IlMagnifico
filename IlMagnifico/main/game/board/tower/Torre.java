913216516a6c74392e9275d574f8785c3d9bf47b
Raruto <Matteo@ASUS-F550L>
2017-05-21 10:29:22 +0200

    minor changes

--- IlMagnifico/main/game/board/tower/Torre.java
+++ IlMagnifico/main/game/board/tower/Torre.java
@@ -1,14 +1,9 @@
 package main.game.board.tower;
 
 import main.game.cards.development.CartaSviluppo;
-import main.game.spaces.Spazio;
 
 public class Torre {
 	private SpazioTorre[] piani;
-
-	public void setCarta(CartaSviluppo carta) {
-
-	}
 
 	public void setCartaPiano(CartaSviluppo carta, int piano) {
 
