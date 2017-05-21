28097159861c79b23ddcb0f4edf5d9b52a3d4872
CharlieChaplin1947 <carlo1.castelnuovo@mail.polimi.it>
2017-05-21 11:39:14 +0200

    Inserito il metodo per la soddisfazione dei costi della carta
    Devo ancora finire di modificare il resto del progetto conseguentemente

--- IlMagnifico/main/game/cards/development/CartaSviluppo.java
+++ IlMagnifico/main/game/cards/development/CartaSviluppo.java
@@ -3,7 +3,9 @@ package main.game.cards.development;
 import java.util.*;
 
 import main.game.activatables.Effetto;
+import main.game.players.PuntiGiocatore;
 import main.game.players.RiservaGiocatore;
+import main.game.players.RisorseGiocatore;
 
 /**
  * Classe che definisce il concetto di CartaSviluppo
@@ -24,6 +26,11 @@ public class CartaSviluppo {
 	 * Costo che il Giocatore deve pagare per poter acquisire la Carta Sviluppo
 	 */
 	protected RiservaGiocatore costo;
+	/*
+	 * Quando le carte richiedono di pagare punti militari è necessario avere un
+	 * tot di punti militari, indicati sulla carta
+	 */
+	protected int sogliaPuntiMilitari;
 
 	/**
 	 * ArrayList contenente tutti gli Effetti Immediati associati alla Carta.
@@ -48,11 +55,12 @@ public class CartaSviluppo {
 	 * @param effettiImmediati
 	 * @param effettiPermanenti
 	 */
-	public CartaSviluppo(String nome, int periodo, RiservaGiocatore costo, ArrayList<Effetto> effettiImmediati,
+	public CartaSviluppo(String nome, int periodo, RiservaGiocatore costo, int sogliaPuntiMilitari, ArrayList<Effetto> effettiImmediati,
 			ArrayList<Effetto> effettiPermanenti) {
 		this.nome = nome;
 		this.periodo = periodo;
 		this.costo = costo;
+		this.sogliaPuntiMilitari=sogliaPuntiMilitari;
 		this.effettiImmediati = effettiImmediati;
 		this.effettiPermanenti = effettiPermanenti;
 	}
@@ -87,5 +95,45 @@ public class CartaSviluppo {
 	 */
 	public RiservaGiocatore getCosto() {
 		return this.costo;
+	}
+
+	/*
+	 * Confronta la riserva del giocatore con i costi della carta. Ritorna true
+	 * se il giocatore puo pagare
+	 */
+	public boolean soddisfacimentoCosti(RiservaGiocatore riserva) {
+		if (confrontaRisorse(riserva.getRisorseGiocatore(), this.costo.getRisorseGiocatore())
+				| confrontaPunti(riserva.getPuntiGiocatore(), this.costo.getPuntiGiocatore()))
+			return true;
+		else
+			return false;
+	}
+
+	/*
+	 * Ritorna true se il giocatore puo pagare i costi in risorse della carta
+	 */
+	public boolean confrontaRisorse(RisorseGiocatore risorse, RisorseGiocatore risorseCosto) {
+		if (risorse.getLegni() >= risorseCosto.getLegni() && risorse.getMonete() >= risorseCosto.getMonete()
+				&& risorseCosto.getPietre() >= risorseCosto.getPietre()
+				&& risorse.getServitori() >= risorseCosto.getServitori())
+			return true;
+		else
+			return false;
+	}
+
+	/*
+	 * Ritorna true se il giocatore puo pagare i costi in punti della carta
+	 */
+	public boolean confrontaPunti(PuntiGiocatore punti, PuntiGiocatore puntiCosto) {
+		// dalle carte ho visto che nei costi non ci sono mai costi in punti che
+		// non siano militari.
+		// Inoltre, quando si deve pagare punti militari, c'è anche una soglia
+		// maggiore di punti da superare per potere pagare
+		if (punti.getPuntiMilitari() >= puntiCosto.getPuntiMilitari()
+				&& punti.getPuntiMilitari() >= this.sogliaPuntiMilitari)
+			return true;
+		else
+			return false;
+
 	}
 }
