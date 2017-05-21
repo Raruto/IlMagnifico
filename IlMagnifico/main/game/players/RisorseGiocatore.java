bbef28f71e6270483c84d4760aaa00140d743f01
Raruto <Matteo@ASUS-F550L>
2017-05-21 09:00:10 +0200

    minor changes

--- IlMagnifico/main/game/players/RisorseGiocatore.java
+++ IlMagnifico/main/game/players/RisorseGiocatore.java
@@ -4,20 +4,20 @@ package main.game.players;
  * Classe per la gestione delle Risorse Giocatore
  */
 public class RisorseGiocatore {
-	private int legno;
+	private int legni;
 	private int monete;
 	private int pietre;
 	private int servitori;
 
-	public RisorseGiocatore(int legno, int monete, int pietre, int servitori) {
-		this.legno = legno;
+	public RisorseGiocatore(int legni, int monete, int pietre, int servitori) {
+		this.legni = legni;
 		this.monete = monete;
 		this.pietre = pietre;
 		this.servitori = servitori;
 	}
 
-	public int getLegno() {
-		return legno;
+	public int getLegni() {
+		return legni;
 	}
 
 	public int getMonete() {
