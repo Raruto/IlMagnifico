c34dde3a20454751e0e6ac436a3c0ff900a5f2f9
CharlieChaplin1947 <carlo1.castelnuovo@mail.polimi.it>
2017-06-01 18:27:35 +0200

    Risistemazione

--- IlMagnifico/main/model/Famigliare.java
+++ IlMagnifico/main/model/Famigliare.java
@@ -88,101 +88,28 @@ public class Famigliare {
 		// 0=territorio, 1=personaggio
 		// 2=edificio, 3=impresa
 
-		for (int i = 4 * identificativoTorre; i < identificativoTorre + 4; i++) {
-			// controllo che il giocatore sulla stessa torre non abbia due
-			// famigliari colorati
-			if ((spazioAzione.getFamigliareTorre(i).getGiocatore() == this.giocatoreAppartenenza)
-					&& (spazioAzione.getFamigliareTorre(i).getNeutralita() == false) && (this.neutro == false))
-				throw new SameTowerException();
-		}
+		controlloSameTower(identificativoTorre, spazioAzione);
 
-		if (identificativoTorre == 0 && this.giocatoreAppartenenza.getPlancia().getTerritori().size() == 6)
-			throw new MaxCardsReachedException();
-		if (identificativoTorre == 1 && this.giocatoreAppartenenza.getPlancia().getPersonaggi().size() == 6)
-			throw new MaxCardsReachedException();
-		if (identificativoTorre == 2 && this.giocatoreAppartenenza.getPlancia().getEdifici().size() == 6)
-			throw new MaxCardsReachedException();
-		if (identificativoTorre == 3 && this.giocatoreAppartenenza.getPlancia().getImprese().size() == 6)
-			throw new MaxCardsReachedException();
+		controlloMaxCardsReached(identificativoTorre);
 
 		// creo un famigliare temporaneo su cui fare tutti i calcoli derivanti
 		// da effetti
-		Famigliare famigliareTemporaneo = new Famigliare(new Giocatore(), this.valore, this.neutro);
-		famigliareTemporaneo.getGiocatore().getRisorse().setLegno(this.giocatoreAppartenenza.getRisorse().getLegno());
-		famigliareTemporaneo.getGiocatore().getRisorse().setPietre(this.giocatoreAppartenenza.getRisorse().getPietre());
-		famigliareTemporaneo.getGiocatore().getRisorse().setMonete(this.giocatoreAppartenenza.getRisorse().getMonete());
-		famigliareTemporaneo.getGiocatore().getRisorse()
-				.setServitori(this.giocatoreAppartenenza.getRisorse().getServitori());
-		famigliareTemporaneo.getGiocatore().getPunti()
-				.setPuntiMilitari(this.giocatoreAppartenenza.getPunti().getPuntiMilitari());
-		famigliareTemporaneo.getGiocatore().getPunti()
-				.setPuntiFede(this.giocatoreAppartenenza.getPunti().getPuntiFede());
-		famigliareTemporaneo.getGiocatore().getPunti()
-				.setPuntiVittoria(this.giocatoreAppartenenza.getPunti().getPuntiVittoria());
-		famigliareTemporaneo.getGiocatore().setColore(this.giocatoreAppartenenza.getColore());
+		Famigliare famigliareTemporaneo = clonaFamigliare();
 
 		// Controllo tutti gli effetti permanenti delle carte personaggio e
 		// delle scomuniche
-		for (int j = 0; j < this.giocatoreAppartenenza.getPlancia().getPersonaggi().size(); j++) {
-			if (identificativoTorre == 0)
-				this.giocatoreAppartenenza.getPlancia().getPersonaggi().get(j).attivaOnAzione(null,
-						EAzioniGiocatore.PrendiTerritorio, famigliareTemporaneo, null);
-			if (identificativoTorre == 1)
-				this.giocatoreAppartenenza.getPlancia().getPersonaggi().get(j).attivaOnAzione(null,
-						EAzioniGiocatore.PrendiPersonaggio, famigliareTemporaneo, null);
-			if (identificativoTorre == 2)
-				this.giocatoreAppartenenza.getPlancia().getPersonaggi().get(j).attivaOnAzione(null,
-						EAzioniGiocatore.PrendiEdificio, famigliareTemporaneo, null);
-			if (identificativoTorre == 3)
-				this.giocatoreAppartenenza.getPlancia().getPersonaggi().get(j).attivaOnAzione(null,
-						EAzioniGiocatore.PrendiImpresa, famigliareTemporaneo, null);
-		}
-
-		if ((famigliareTemporaneo.neutro == false) && (this.giocatoreAppartenenza.getScomunica(0) != null))
-			this.giocatoreAppartenenza.getScomunica(0).attivaOnAzione(null, EAzioniGiocatore.MuoviColorato,
-					famigliareTemporaneo, null);
-
-		if (this.giocatoreAppartenenza.getScomunica(1) != null) {
-			if (identificativoTorre == 0)
-				this.giocatoreAppartenenza.getScomunica(1).attivaOnAzione(null, EAzioniGiocatore.PrendiTerritorio,
-						famigliareTemporaneo, null);
-			if (identificativoTorre == 1)
-				this.giocatoreAppartenenza.getScomunica(1).attivaOnAzione(null, EAzioniGiocatore.PrendiPersonaggio,
-						famigliareTemporaneo, null);
-			if (identificativoTorre == 2)
-				this.giocatoreAppartenenza.getScomunica(1).attivaOnAzione(null, EAzioniGiocatore.PrendiEdificio,
-						famigliareTemporaneo, null);
-			if (identificativoTorre == 3)
-				this.giocatoreAppartenenza.getScomunica(1).attivaOnAzione(null, EAzioniGiocatore.PrendiImpresa,
-						famigliareTemporaneo, null);
-		}
+		controlloMalusEffettiPermanentiTorre(identificativoTorre, famigliareTemporaneo);
+		controlloMalusScomunicheTorre(identificativoTorre, famigliareTemporaneo);
 
 		// controllo che il famigliare abbia un valore sufficiente per l'azione
 		if (famigliareTemporaneo.valore < (1 + 2 * (posizione % 4)))
 			throw new InsufficientValueException();
 
 		// guardo se c'è un'altra pedina e nel caso pago 3 monete
-		for (int i = 4 * identificativoTorre; i < identificativoTorre + 4; i++) {
-			if (spazioAzione.torreLibera(i) == false) {
-				if (famigliareTemporaneo.giocatoreAppartenenza.getRisorse().getMonete() < 3)
-					throw new NoMoneyException();
-				else {
-					famigliareTemporaneo.giocatoreAppartenenza.getRisorse().cambiaMonete(-3);
-					break;
-				}
-			}
-		}
+		controlloDelleTreMonete(identificativoTorre, famigliareTemporaneo, spazioAzione);
 
 		// Applico l'eventuale bonus della torre
-		boolean controllo = false;
-		if (posizione % 4 == 3 | posizione % 4 == 2) {
-			for (int i = 0; i < this.giocatoreAppartenenza.getPlancia().getPersonaggi().size(); i++) {
-				if (this.giocatoreAppartenenza.getPlancia().getPersonaggi().get(i).attivaOnEffettoTorre())
-					controllo = true;
-			}
-			if (controllo == false)
-				spazioAzione.eseguiEffettoImmediatoTorre(famigliareTemporaneo.giocatoreAppartenenza, posizione);
-		}
+		controlloEffettoImmediatoTorre(posizione, famigliareTemporaneo, spazioAzione);
 
 		// controllo le carte personaggio per applicare eventuali sconti
 		for (int j = 0; j < this.giocatoreAppartenenza.getPlancia().getPersonaggi().size(); j++) {
@@ -253,6 +180,160 @@ public class Famigliare {
 						this, null);
 			}
 			spazioAzione.setFamigliareTorre(this, posizione);
+		}
+	}
+
+	/**
+	 * Metodo che controlla che sulla stessa torre non ci siano due famigliari
+	 * dello stesso colore
+	 * 
+	 * @param
+	 * @return
+	 */
+	public void controlloSameTower(int identificativoTorre, SpazioAzione spazioAzione) throws SameTowerException {
+		for (int i = 4 * identificativoTorre; i < identificativoTorre + 4; i++) {
+			// controllo che il giocatore sulla stessa torre non abbia due
+			// famigliari colorati
+			if ((spazioAzione.getFamigliareTorre(i).getGiocatore() == this.giocatoreAppartenenza)
+					&& (spazioAzione.getFamigliareTorre(i).getNeutralita() == false) && (this.neutro == false))
+				throw new SameTowerException();
+		}
+	}
+
+	/**
+	 * Metodo che controlla che un giocatore non abbia già il massimo numero di
+	 * carte del tipo della carta che vuole prendere
+	 * 
+	 * @param
+	 * @return
+	 */
+	public void controlloMaxCardsReached(int identificativoTorre) throws MaxCardsReachedException {
+		if (identificativoTorre == 0 && this.giocatoreAppartenenza.getPlancia().getTerritori().size() == 6)
+			throw new MaxCardsReachedException();
+		if (identificativoTorre == 1 && this.giocatoreAppartenenza.getPlancia().getPersonaggi().size() == 6)
+			throw new MaxCardsReachedException();
+		if (identificativoTorre == 2 && this.giocatoreAppartenenza.getPlancia().getEdifici().size() == 6)
+			throw new MaxCardsReachedException();
+		if (identificativoTorre == 3 && this.giocatoreAppartenenza.getPlancia().getImprese().size() == 6)
+			throw new MaxCardsReachedException();
+	}
+
+	/**
+	 * Metodo che crea una copia del famigliare
+	 * 
+	 * @param
+	 * @return
+	 */
+	public Famigliare clonaFamigliare() {
+		Famigliare famigliareTemporaneo = new Famigliare(new Giocatore(), this.valore, this.neutro);
+		famigliareTemporaneo.getGiocatore().getRisorse().setLegno(this.giocatoreAppartenenza.getRisorse().getLegno());
+		famigliareTemporaneo.getGiocatore().getRisorse().setPietre(this.giocatoreAppartenenza.getRisorse().getPietre());
+		famigliareTemporaneo.getGiocatore().getRisorse().setMonete(this.giocatoreAppartenenza.getRisorse().getMonete());
+		famigliareTemporaneo.getGiocatore().getRisorse()
+				.setServitori(this.giocatoreAppartenenza.getRisorse().getServitori());
+		famigliareTemporaneo.getGiocatore().getPunti()
+				.setPuntiMilitari(this.giocatoreAppartenenza.getPunti().getPuntiMilitari());
+		famigliareTemporaneo.getGiocatore().getPunti()
+				.setPuntiFede(this.giocatoreAppartenenza.getPunti().getPuntiFede());
+		famigliareTemporaneo.getGiocatore().getPunti()
+				.setPuntiVittoria(this.giocatoreAppartenenza.getPunti().getPuntiVittoria());
+		famigliareTemporaneo.getGiocatore().setColore(this.giocatoreAppartenenza.getColore());
+		return famigliareTemporaneo;
+	}
+
+	/**
+	 * Metodo che controlla i malus derivanti da effetti permanenti delle carte
+	 * quando eseguo uno spostamento su una torre
+	 * 
+	 * @param
+	 * @return
+	 */
+	public void controlloMalusEffettiPermanentiTorre(int identificativoTorre, Famigliare famigliare) {
+		for (int j = 0; j < this.giocatoreAppartenenza.getPlancia().getPersonaggi().size(); j++) {
+			if (identificativoTorre == 0)
+				this.giocatoreAppartenenza.getPlancia().getPersonaggi().get(j).attivaOnAzione(null,
+						EAzioniGiocatore.PrendiTerritorio, famigliare, null);
+			if (identificativoTorre == 1)
+				this.giocatoreAppartenenza.getPlancia().getPersonaggi().get(j).attivaOnAzione(null,
+						EAzioniGiocatore.PrendiPersonaggio, famigliare, null);
+			if (identificativoTorre == 2)
+				this.giocatoreAppartenenza.getPlancia().getPersonaggi().get(j).attivaOnAzione(null,
+						EAzioniGiocatore.PrendiEdificio, famigliare, null);
+			if (identificativoTorre == 3)
+				this.giocatoreAppartenenza.getPlancia().getPersonaggi().get(j).attivaOnAzione(null,
+						EAzioniGiocatore.PrendiImpresa, famigliare, null);
+		}
+	}
+
+	/**
+	 * Metodo che controlla i malus derivanti dalle tessere scomunica in
+	 * possesso del giocatore del famigliare
+	 * 
+	 * @param
+	 * @return
+	 * 
+	 */
+	public void controlloMalusScomunicheTorre(int identificativoTorre, Famigliare famigliare) {
+		if ((famigliare.neutro == false) && (this.giocatoreAppartenenza.getScomunica(0) != null))
+			this.giocatoreAppartenenza.getScomunica(0).attivaOnAzione(null, EAzioniGiocatore.MuoviColorato, famigliare,
+					null);
+
+		if (this.giocatoreAppartenenza.getScomunica(1) != null) {
+			if (identificativoTorre == 0)
+				this.giocatoreAppartenenza.getScomunica(1).attivaOnAzione(null, EAzioniGiocatore.PrendiTerritorio,
+						famigliare, null);
+			if (identificativoTorre == 1)
+				this.giocatoreAppartenenza.getScomunica(1).attivaOnAzione(null, EAzioniGiocatore.PrendiPersonaggio,
+						famigliare, null);
+			if (identificativoTorre == 2)
+				this.giocatoreAppartenenza.getScomunica(1).attivaOnAzione(null, EAzioniGiocatore.PrendiEdificio,
+						famigliare, null);
+			if (identificativoTorre == 3)
+				this.giocatoreAppartenenza.getScomunica(1).attivaOnAzione(null, EAzioniGiocatore.PrendiImpresa,
+						famigliare, null);
+		}
+	}
+
+	/**
+	 * Metodo che controlla che il giocatore abbia le tre monete da pagare nel
+	 * caso in cui nella stessa torre sia presente un altro famigliare
+	 * 
+	 * @param
+	 * @return
+	 * 
+	 */
+	public void controlloDelleTreMonete(int identificativoTorre, Famigliare famigliareTemporaneo,
+			SpazioAzione spazioAzione) throws NoMoneyException {
+		for (int i = 4 * identificativoTorre; i < identificativoTorre + 4; i++) {
+			if (spazioAzione.torreLibera(i) == false) {
+				if (famigliareTemporaneo.giocatoreAppartenenza.getRisorse().getMonete() < 3)
+					throw new NoMoneyException();
+				else {
+					famigliareTemporaneo.giocatoreAppartenenza.getRisorse().cambiaMonete(-3);
+					break;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Metodo che controlla che sulla posizione dove viene posizionato il
+	 * famigliare ci sia un bonus. Se c'è lo attiva
+	 * 
+	 * @param
+	 * @return
+	 * 
+	 */
+	public void controlloEffettoImmediatoTorre(int posizione, Famigliare famigliareTemporaneo,
+			SpazioAzione spazioAzione) {
+		boolean controllo = false;
+		if (posizione % 4 == 3 | posizione % 4 == 2) {
+			for (int i = 0; i < this.giocatoreAppartenenza.getPlancia().getPersonaggi().size(); i++) {
+				if (this.giocatoreAppartenenza.getPlancia().getPersonaggi().get(i).attivaOnEffettoTorre())
+					controllo = true;
+			}
+			if (controllo == false)
+				spazioAzione.eseguiEffettoImmediatoTorre(famigliareTemporaneo.giocatoreAppartenenza, posizione);
 		}
 	}
 
