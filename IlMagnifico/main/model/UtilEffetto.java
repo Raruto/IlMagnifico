8cae14d208fd157f346d10115200a4b7dbfa6ad5
CharlieChaplin1947 <carlo1.castelnuovo@mail.polimi.it>
2017-06-03 15:11:01 +0200

    Riorganizzazione degli effetti in UtilEffetto
    Tolti effetti inutili e sistemate le posizioni dei parametri

--- IlMagnifico/main/model/UtilEffetto.java
+++ IlMagnifico/main/model/UtilEffetto.java
@@ -4,24 +4,21 @@ public class UtilEffetto {
 	/**
 	 * Aggiunge risorse al giocatore passato tra i parametri. Tra i parametri
 	 * sono passate le risorse da aggiungere e, se negativi, sono quelle che il
-	 * giocatore deve spendere. L'array in ingresso ha come valori
-	 * rispettivamente: Giocatore e vari int che simboleggiano i delta di
-	 * monete, legno, pietre, servitori, punti vittoria, punti militari e punti
-	 * fede. Restituisce true se l'esito del metodo e' positivo, false
-	 * altrimenti.
+	 * giocatore deve spendere.Restituisce true se l'esito del metodo e'
+	 * positivo, false altrimenti.
 	 * 
 	 * @param
 	 * @return boolean
 	 */
 	public boolean aggiungiRisorse(Object o[]) {// metodo numero 0
 		Giocatore giocatore = (Giocatore) (o[1]);
-		int monete = (int) (o[2]);
-		int legno = (int) (o[3]);
-		int pietre = (int) (o[4]);
-		int servitori = (int) (o[5]);
-		int puntiVittoria = (int) (o[6]);
-		int puntiMilitari = (int) (o[7]);
-		int puntiFede = (int) (o[8]);
+		int monete = (int) (o[5]);
+		int legno = (int) (o[6]);
+		int pietre = (int) (o[7]);
+		int servitori = (int) (o[8]);
+		int puntiVittoria = (int) (o[9]);
+		int puntiMilitari = (int) (o[10]);
+		int puntiFede = (int) (o[11]);
 
 		int[] risorse = { monete, legno, pietre, servitori, puntiVittoria, puntiMilitari, puntiFede };
 		int[] risorseGiocatore = { giocatore.getRisorse().getMonete(), giocatore.getRisorse().getLegno(),
@@ -50,19 +47,17 @@ public class UtilEffetto {
 
 	/**
 	 * Controlla se il giocatore puo' pagare le risorse per l'aquisizione di una
-	 * carta. L'array in ingresso ha come valori (dalla posizione 1 in poi):
-	 * Giocatore e piu' int rispettivamente per: monete,legno,pietre,servitori
-	 * indicati nel costo della carta
+	 * carta.
 	 * 
 	 * @param
 	 * @return boolean
 	 */
 	public boolean cartaAcquisibileRisorse(Object o[]) {// metodo numero 1
 		Giocatore giocatore = (Giocatore) (o[1]);
-		int monete = (int) (o[2]);
-		int legno = (int) (o[3]);
-		int pietre = (int) (o[4]);
-		int servitori = (int) (o[5]);
+		int monete = (int) (o[5]);
+		int legno = (int) (o[6]);
+		int pietre = (int) (o[7]);
+		int servitori = (int) (o[8]);
 		if (giocatore.getRisorse().getMonete() >= monete && giocatore.getRisorse().getLegno() >= legno
 				&& giocatore.getRisorse().getPietre() >= pietre && giocatore.getRisorse().getServitori() >= servitori)
 			return true;
@@ -71,17 +66,15 @@ public class UtilEffetto {
 	}
 
 	/**
-	 * Controlla se il giocatore puo' pagare i punti militari per
-	 * l'aquisizione di una carta. L'array in ingresso ha come valori
-	 * rispettivamente: Giocatore e la soglia dei punti militari da avere per
-	 * potere pagare
+	 * Controlla se il giocatore puo' pagare i punti militari per l'aquisizione
+	 * di una carta.
 	 * 
 	 * @param
 	 * @return boolean
 	 */
 	public boolean cartaAcquisibilePunti(Object o[]) {// metodo numero 2
 		Giocatore giocatore = (Giocatore) (o[1]);
-		int puntiMilitari = (int) (o[2]);
+		int puntiMilitari = (int) (o[10]);
 		if (giocatore.getPunti().getPuntiMilitari() >= puntiMilitari)
 			return true;
 		else
@@ -92,8 +85,8 @@ public class UtilEffetto {
 	 * Proposta primitiva del metodo per l'esecuzione del privilegio del
 	 * consiglio. Tramite l'array in ingresso e' possibile indicare quanti
 	 * privilegi del consiglio effettuare ed il metodo procede da solo al
-	 * controllo sulle scelte. Quando e' richiesta l'interazione dell'utente
-	 * e' segnalato con un commento
+	 * controllo sulle scelte. Quando e' richiesta l'interazione dell'utente e'
+	 * segnalato con un commento
 	 */
 	public void eseguiPrivilegioDelConsiglio(Object o[]) {// metodo numero 3
 		Giocatore giocatore = (Giocatore) (o[1]);
@@ -146,96 +139,23 @@ public class UtilEffetto {
 	 * @param
 	 * @return
 	 */
-	public void aumentaDiDueAzioneTerritorio(Object o[]) {// metodo numero 4
-		int posizioneTorre = (int) (o[1]);
-		Famigliare famigliare = (Famigliare) (o[2]);
-		if (posizioneTorre >= 0 && posizioneTorre <= 3)
-			famigliare.cambiaValore(2);
-	}
-
-	/**
-	 * Controlla se l'azione effetuata e' sulla torre edificio ed in caso
-	 * affermativo aumenta il valore del famigliare passato in ingresso di due.
-	 * I valore in ingresso sono la posizione dell'azione nelle torri e la
-	 * pedina mossa
-	 * 
-	 * @param
-	 * @return
-	 */
-	public void aumentaDiDueAzioneEdificio(Object o[]) {// metodo numero 5
-		int posizioneTorre = (int) (o[1]);
-		Famigliare famigliare = (Famigliare) (o[2]);
-		if (posizioneTorre >= 8 && posizioneTorre <= 11)
-			famigliare.cambiaValore(2);
-	}
-
-	/**
-	 * Controlla se l'azione effetuata e' sulla torre personaggio ed in caso
-	 * affermativo aumenta il valore del famigliare passato in ingresso di due.
-	 * I valore in ingresso sono la posizione dell'azione nelle torri e la
-	 * pedina mossa
-	 * 
-	 * @param
-	 * @return
-	 */
-	public void aumentaDiDueAzionePersonaggio(Object o[]) {// metodo numero 6
-		int posizioneTorre = (int) (o[1]);
-		Famigliare famigliare = (Famigliare) (o[2]);
-		if (posizioneTorre >= 4 && posizioneTorre <= 7)
-			famigliare.cambiaValore(2);
-	}
-
-	/**
-	 * Controlla se l'azione effetuata e' sulla torre impresa ed in caso
-	 * affermativo aumenta il valore del famigliare passato in ingresso di due.
-	 * I valore in ingresso sono la posizione dell'azione nelle torri e la
-	 * pedina mossa
-	 * 
-	 * @param
-	 * @return
-	 */
-	public void aumentaDiDueAzioneImpresa(Object o[]) {// metodo numero 7
-		int posizioneTorre = (int) (o[1]);
-		Famigliare famigliare = (Famigliare) (o[2]);
-		if (posizioneTorre >= 12 && posizioneTorre <= 15)
-			famigliare.cambiaValore(2);
-	}
-
-	/**
-	 * Metodo per gli effetti in cui si riceve o spende risorse e si riceve uno
-	 * o piu' privilegi del consiglio. I parametri in ingresso sono il
-	 * giocatore, unita' di monete, legna, pietre, servitori, punti
-	 * vittoria, punti militari , punti fede ed il numero di privilegi del
-	 * consiglio. Se i parametri sono negativi si procede al controllo se il
-	 * giocatore puo' pagare le risorse.
-	 * 
-	 * @param
-	 * @return
-	 */
-	public void aggiungiRisorseEPrivilegioDelConsiglio(Object o[]) {// metodo
-																	// numero 8
-		boolean controllo;
-		Object[] parametri = new Object[3];
-		controllo = aggiungiRisorse(o);
-		if (controllo == true) {
-			parametri[1] = o[1];
-			parametri[9] = o[9];
-			eseguiPrivilegioDelConsiglio(parametri);
-		}
+	public void aumentaDiDueAzione(Object o[]) {// metodo numero 4
+		Famigliare famigliare = (Famigliare) (o[3]);
+		famigliare.cambiaValore(2);
 	}
 
 	/**
 	 * Metodo per gli effetti immediati che richiedono di eseguire l'azione
 	 * Raccolto. I parametri passati in ingresso sono il giocatore e il valore
 	 * della azioneraccolto che si va a fare. NB:al momento della scrittura non
-	 * e' ancora stato implementato il metodo che gestisce il raccolto, per
-	 * cui tale metodo viene ipotizzato
+	 * e' ancora stato implementato il metodo che gestisce il raccolto, per cui
+	 * tale metodo viene ipotizzato
 	 * 
 	 * @param
 	 * @return
 	 * 
 	 */
-	public void eseguiEffettoImmediatoRaccolto(Object o[]) {// metodo numero 9
+	public void eseguiEffettoImmediatoRaccolto(Object o[]) {// metodo numero 5
 		Giocatore giocatore = (Giocatore) (o[1]);
 		int valore = (int) (o[2]);
 		giocatore.raccolto(valore);// metodo non ancora implementato. In ogni
@@ -247,64 +167,21 @@ public class UtilEffetto {
 	 * Metodo per gli effetti immediati che richiedono di eseguire l'azione
 	 * Produzione. I parametri passati in ingresso sono il giocatore e il valore
 	 * della azione produzione che si va a fare. NB:al momento della scrittura
-	 * non e' ancora stato implementato il metodo che gestisce la
-	 * produzione, per cui tale metodo viene ipotizzato
+	 * non e' ancora stato implementato il metodo che gestisce la produzione,
+	 * per cui tale metodo viene ipotizzato
 	 * 
 	 * @param
 	 * @return
 	 * 
 	 */
-	public void eseguiEffettoImmediatoProduzione(Object o[]) {// metodo numero
-																// 10
+	public void eseguiEffettoImmediatoProduzione(Object o[]) {// metodo numero 6
+
 		Giocatore giocatore = (Giocatore) (o[1]);
 		int valore = (int) (o[2]);
 		giocatore.produzione(valore);// metodo non ancora implementato. In ogni
 										// caso verra' chiamato il metodo
 										// che
 										// gestisce la produzione del giocatore
-	}
-
-	/**
-	 * Metodo per effetti immediati dove si ricevono risorse e si effettua una
-	 * azione di raccolto. I parametri passati sono rispettivamente: giocatore,
-	 * unita' di monete, legno, pietre, servitori, punti vittoria, punti
-	 * militari, punti fede ed il valore dell'azione raccolto che si va a
-	 * svolgere
-	 * 
-	 * @param
-	 * @return
-	 */
-	public void aggiungiRisorseEEseguiRaccolto(Object o[]) {// metodo numero 11
-		boolean controllo;
-		Object[] parametri = new Object[3];
-		controllo = aggiungiRisorse(o);
-		if (controllo == true) {
-			parametri[1] = o[1];
-			parametri[2] = o[9];
-			eseguiEffettoImmediatoRaccolto(parametri);
-		}
-	}
-
-	/**
-	 * Metodo per effetti immediati dove si ricevono risorse e si effettua una
-	 * azione di produzione. I parametri passati sono rispettivamente:
-	 * giocatore, unita' di monete, legno, pietre, servitori, punti
-	 * vittoria, punti militari, punti fede ed il valore dell'azione produzione
-	 * che si va a svolgere
-	 * 
-	 * @param
-	 * @return
-	 */
-	public void aggiungiRisorseEEseguiProduzione(Object o[]) {// metodo numero
-																// 12
-		boolean controllo;
-		Object[] parametri = new Object[3];
-		controllo = aggiungiRisorse(o);
-		if (controllo == true) {
-			parametri[1] = o[1];
-			parametri[2] = o[9];
-			eseguiEffettoImmediatoProduzione(parametri);
-		}
 	}
 
 }
