3f64e256e6cfaaf878459b9bf31f80c124c95355
CharlieChaplin1947 <carlo1.castelnuovo@mail.polimi.it>
2017-06-08 16:07:04 +0200

    aggiunte le descrizioni

--- IlMagnifico/main/model/enums/ECostiCarte.java
+++ IlMagnifico/main/model/enums/ECostiCarte.java
@@ -9,20 +9,20 @@ public enum ECostiCarte {
 			0,"paga:1 legna,3 pietra"), TEATRO("teatro", 2, 2, 0, 0, 0, 0,"paga:2 monete, 2 legna"), ARCO_DI_TRIONFO("arco di trionfo", 2, 0, 2, 0, 0, 0,"paga:2 monete,2 pietra"), TAGLIAPIETRE(
 					"tagliapietre", 1, 0, 2, 0, 0, 0,"paga:1 monete, 2 pietra"), RESIDENZA("residenza", 0, 0, 2, 0, 0, 0,"paga:2 pietra"), FALEGNAMERIA(
 							"falegnameria", 1, 2, 0, 0, 0, 0,"paga:1 monete,2 legna"), SOSTEGNO_AL_VESCOVO("sostegno al vescovo", 2, 1, 1, 0, 2,
-									4,""), CAMPAGNA_MILITARE("campagna militare", 0, 0, 0, 0, 2, 3,""),
+									4,"paga:2 monete,1 legna, 1 pietra oppure se hai 4 Punti vittoria pagane 2"), CAMPAGNA_MILITARE("campagna militare", 0, 0, 0, 0, 2, 3,"paga:se hai 3 punti militari pagane 2"),
 	// TODO:devo implementare il controllo che i pm da pagare non sono quelli su
 	// cui si fa il controllo dei costi
-	INNALZARE_UNA_STATUA("innalzare una statua", 0, 2, 2, 0, 0, 0,""), COSTRUIRE_LE_MURA("costruire le mura", 0, 0, 3, 0,
-			0, 0,""), COMBATTERE_LE_ERESIE("combattere le eresie", 0, 0, 0, 0, 3, 5,""), INGAGGIARE_RECLUTE(
+	INNALZARE_UNA_STATUA("innalzare una statua", 0, 2, 2, 0, 0, 0,"paga:2 legna,2 pietra"), COSTRUIRE_LE_MURA("costruire le mura", 0, 0, 3, 0,
+			0, 0,"paga 3 pietre"), COMBATTERE_LE_ERESIE("combattere le eresie", 0, 0, 0, 0, 3, 5,"paga:se hai 5 punti militari pagane 3"), INGAGGIARE_RECLUTE(
 					"ingaggiare reclute", 4, 0, 0, 0, 0,
-					0,""), OSPITARE_I_MENDICANTI("ospitare i mendicanti", 0, 3, 0, 0, 0, 0,""), RIPARARE_LA_CHIESA(
+					0,"paga:4 monete"), OSPITARE_I_MENDICANTI("ospitare i mendicanti", 0, 3, 0, 0, 0, 0,"paga:3 legna"), RIPARARE_LA_CHIESA(
 							"riparare la chiesa", 1, 1, 1, 0, 0,
-							0,""), CONTADINO("contadino", 3, 0, 0, 0, 0, 0,""), CONDOTTIERO("condottiero", 2, 0, 0, 0, 0,
-									0,""), ARTIGIANO("artigiano", 3, 0, 0, 0, 0, 0,""), DAMA("dama", 4, 0, 0, 0, 0,
-											0,""), BADESSA("badessa", 3, 0, 0, 0, 0, 0,""), CAVALIERE("cavaliere", 2, 0, 0, 0,
-													0, 0,""), PREDICATORE("predicatore", 2, 0, 0, 0, 0, 0,""), COSTRUTTORE(
+							0,"paga:1 monete,1 legna,1 pietra"), CONTADINO("contadino", 3, 0, 0, 0, 0, 0,"paga:3 monete"), CONDOTTIERO("condottiero", 2, 0, 0, 0, 0,
+									0,"paga:2 monete"), ARTIGIANO("artigiano", 3, 0, 0, 0, 0, 0,"paga:3 monete"), DAMA("dama", 4, 0, 0, 0, 0,
+											0,"paga: 4 monete"), BADESSA("badessa", 3, 0, 0, 0, 0, 0,"paga:3 monete"), CAVALIERE("cavaliere", 2, 0, 0, 0,
+													0, 0,"paga:2 monete"), PREDICATORE("predicatore", 2, 0, 0, 0, 0, 0,"paga:2 monete"), COSTRUTTORE(
 															"costruttore", 4, 0, 0, 0, 0,
-															0,""), FORESTA("foresta", 0, 0, 0, 0, 0, 0,""), BOSCO("bosco", 0,
+															0,"paga:4 monete"), FORESTA("foresta", 0, 0, 0, 0, 0, 0,""), BOSCO("bosco", 0,
 																	0, 0, 0, 0,
 																	0,""), ROCCA("rocca", 0, 0, 0, 0, 0, 0,""), BORGO("borgo",
 																			0, 0, 0, 0, 0, 0,""), CAVA_DI_GHIAIA(
