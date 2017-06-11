73ffc84e936329ed4c6521b53cd3d2f1abed4ecb
Raruto <Matteo@ASUS-F550L>
2017-06-11 22:37:43 +0200

    Deleted unnecessary things, sniff!

--- CodeSamples/PerimetroPoligoni/src/program/Polygon.java
+++
@@ -1,5 +0,0 @@
-package program;
-
-public abstract class Polygon {
-	public abstract double perimeter();
-}
--- CodeSamples/RockPaperScissors/src/Program/Program.java
+++
@@ -1,7 +0,0 @@
-package Program;
-
-public class Program {
-	public static void main(String[] args) {
-		new Gameplay(2).start();
-	}
-}
