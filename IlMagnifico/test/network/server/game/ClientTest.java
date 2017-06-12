e6d1ecb100a6ba9bac71b970f2bad8473944e1ea
CharlieChaplin1947 <carlo1.castelnuovo@mail.polimi.it>
2017-06-12 19:55:06 +0200

    ulteriore test sul client

--- IlMagnifico/test/network/server/game/ClientTest.java
+++ IlMagnifico/test/network/server/game/ClientTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import main.model.enums.EAzioniGiocatore;
 import main.model.enums.EColoriPedine;
+import main.model.enums.EEffettiPermanenti;
 import main.model.enums.ESceltePrivilegioDelConsiglio;
 import main.network.client.Client;
 import main.network.client.ClientException;
@@ -15,8 +16,10 @@ import main.util.Costants;
 
 public class ClientTest {
 
-	/**Test che verifica il funzionamento della comunicazione nell
-	 * */
+	/**
+	 * Test che verifica il funzionamento della comunicazione nel metodo
+	 * relativo allo spostamento di un famigliare su una zona mercato
+	 */
 	@Test
 	public void testMovePawnMarket() {
 		try {
@@ -27,6 +30,7 @@ public class ClientTest {
 			e.printStackTrace();
 		}
 		String nomeGiocatore = "";
+		String nomeLastUpdate = "";
 		try {
 			Client client1 = new Client();
 			client1.startClient("RMI", Costants.SERVER_ADDRESS, Costants.SOCKET_PORT, Costants.RMI_PORT);
@@ -38,16 +42,54 @@ public class ClientTest {
 				nomeGiocatore = client1.getNickname();
 				client1.movePawn(EAzioniGiocatore.Mercato, EColoriPedine.Nera, 1,
 						new ESceltePrivilegioDelConsiglio[] { null, null });
-				assertTrue(nomeGiocatore.equals(client1.getLatestUpdate().getNomeGiocatore()));
+				nomeLastUpdate = client1.getLatestUpdate().getNomeGiocatore();
 			} else if (client2.getNickname().equals(client2.getPlayerTurn())) {
 				nomeGiocatore = client2.getNickname();
 				client2.movePawn(EAzioniGiocatore.Mercato, EColoriPedine.Nera, 1,
 						new ESceltePrivilegioDelConsiglio[] { null, null });
-				assertTrue(nomeGiocatore.equals(client2.getLatestUpdate().getNomeGiocatore()));
+				nomeLastUpdate = client2.getLatestUpdate().getNomeGiocatore();
 			}
 		} catch (ClientException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		assertTrue(nomeGiocatore.equals(nomeLastUpdate));
 	}
+
+	@Test
+	public void testMovePawnHarvest() {
+		try {
+			Server server = new Server();
+			server.startServer(Costants.SOCKET_PORT, Costants.RMI_PORT);
+		} catch (ServerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String nomeGiocatore = "";
+		String nomeLastUpdate = "";
+		try {
+			Client client1 = new Client();
+			client1.startClient("RMI", Costants.SERVER_ADDRESS, Costants.SOCKET_PORT, Costants.RMI_PORT);
+			client1.loginPlayer("primo");
+			Client client2 = new Client();
+			client2.startClient("SOCKET", Costants.SERVER_ADDRESS, Costants.SOCKET_PORT, Costants.RMI_PORT);
+			client2.loginPlayer("secondo");
+			if (client1.getNickname().equals(client1.getPlayerTurn())) {
+				nomeGiocatore = client1.getNickname();
+				client1.movePawn(EAzioniGiocatore.Raccolto, EColoriPedine.Nera, 1,
+						new EEffettiPermanenti[] { null, null });
+				nomeLastUpdate = client1.getLatestUpdate().getNomeGiocatore();
+			} else if (client2.getNickname().equals(client2.getPlayerTurn())) {
+				nomeGiocatore = client2.getNickname();
+				client2.movePawn(EAzioniGiocatore.Raccolto, EColoriPedine.Nera, 1,
+						new EEffettiPermanenti[] { null, null });
+				nomeLastUpdate = client2.getLatestUpdate().getNomeGiocatore();
+			}
+		} catch (ClientException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertTrue(nomeGiocatore.equals(nomeLastUpdate));
+	}
+	
 }
