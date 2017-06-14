56907bc0b78352cc99f43070ed343d0e76513572
Raruto <Matteo@ASUS-F550L>
2017-06-14 08:55:20 +0200

    Added UI launcher

---
+++ IlMagnifico/main/ui/StartOneUI.java
@@ -0,0 +1,28 @@
+package main.ui;
+
+import java.util.Scanner;
+
+import main.ui.cli.FakeUI;
+import main.ui.gui.Frame;
+import main.util.Costants;
+
+public class StartOneUI {
+	private static Object inText;
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.print("Start [C]LI or [G]UI? (Default: [C]): ");
+		inText = scanner.nextLine().toUpperCase();
+
+		if (inText.equals("G")) {
+			Frame.main(args);
+		} else if (inText.equals("C")) {
+			FakeUI.mainClient(Costants.SERVER_ADDRESS, Costants.SOCKET_PORT, Costants.RMI_PORT);
+		}
+		// Default: Client
+		else {
+			System.out.println("Starting CLI..");
+			FakeUI.mainClient(Costants.SERVER_ADDRESS, Costants.SOCKET_PORT, Costants.RMI_PORT);
+		}
+	}
+}
