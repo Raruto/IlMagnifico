bd2fce4e430598d32c581e9cfd3f637e04b83cb4
Raruto <Matteo@ASUS-F550L>
2017-06-18 11:48:34 +0200

    TODO: finire EEffettiScomuniche

--- IlMagnifico/main/model/enums/EEffettiScomuniche.java
+++ IlMagnifico/main/model/enums/EEffettiScomuniche.java
@@ -2,9 +2,46 @@ package main.model.enums;
 
 public enum EEffettiScomuniche {
 
-	RICEVI_MENO_PM(				"ricevi meno pm", 			  -1, EAzioniGioco.RiceviPM, 			  0, 0, 0, 0, 0, -1, 0, 0), 
-	MENO_QUATTRO_ON_TERRITORIO( "meno quattro on territorio", 18, EAzioniGioco.PrendiTerritorio, 	  0, 0, 0, 0, 0, 0, 0, 0), 
-	NO_PV_PERSONAGGIO(			"no pv personaggio", 		  -1, EAzioniGioco.PersonaggiFinePartita, 0, 0, 0, 0, 0, 0, 0, 0);
+	//Prima fila
+	RICEVI_MENO_PM(				   "Receive less MP", 			 -1, EAzioniGioco.RiceviPM, 			  0, 0, 0, 0, 0, -1, 0, 0), 
+	MENO_QUATTRO_ON_TERRITORIO(    "-4 on Territory", 			 18, EAzioniGioco.PrendiTerritorio, 	  0, 0, 0, 0, 0, 0, 0, 0), 
+	NO_PV_PERSONAGGIO(			   "No Charachter VP", 		     -1, EAzioniGioco.PersonaggiFinePartita, 0, 0, 0, 0, 0, 0, 0, 0),
+	
+	//;
+	//Seconda fila
+	RICEVI_MENO_MONETE(			   "Receive less C", 	 		 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	MENO_QUATTRO_ON_EDIFICIO(	   "-4 on Building", 	 		 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	NO_PV_IMPRESA(				   "No Venture VP", 	 	 	 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	
+	//;
+	//Terza fila
+	RICEVI_MENO_SERVITORI(		   "Receive less Sv", 	 		 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	MENO_QUATTRO_ON_PERSONAGGIO(   "-4 on Charachter",	 		 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	NO_PV_TERRITORIO(			   "No Territory VP", 	 		 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	
+	//;
+	//Quarta fila
+	RICEVI_MENO_LEGNA_PIETRA(	   "Receive less W, St",	 	 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	MENO_QUATTRO_ON_IMPRESA(	   "-4 on Venture",	 	 		 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	PV_MENO_CINQUE_PV(			   "VP-5 foreach VP",	 		 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	
+	//;
+	//Quinta fila
+	MENO_TRE_ON_RACCOLTO(		   "-3 on Harvest",	 	 		 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	NO_MERCATO(					   "No Market",	 	 	 		 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	PV_MENO_UNO_PM(				   "VP-1 foreach MP",	 		 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+
+	//;
+	//Sesta fila
+	MENO_TRE_ON_PRODUZIONE(		   "-3 on Production",	 		 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	DIVISO_DUE_ON_INCREMENTO( 	   "Sv/2 on Increment", 	 	 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	PV_MENO_UNO_LEGNO_PIETRA_CARTE("VP-1 foreach W, St cards",	 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	
+	//;
+	//Settima fila
+	MENO_UNO_ON_FAMIGLARI(		   "-1 on Familiars",	 		 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	SALTA_PRIMA_AZIONE_TURNO( 	   "Skip the first move", 		 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio),
+	PV_MENO_UNO_RISERVA(	  	   "VP-1 foreach W, St, Sv, C",	 numeroEffetto, EAzioniGioco, monete, legno, pietra, servitori, PV, PM, PF, privilegiConsiglio);
 
 	private String nome;
 	private int numeroEffetto;
--- IlMagnifico/main/model/enums/EScomuniche.java
+++ IlMagnifico/main/model/enums/EScomuniche.java
@@ -3,9 +3,48 @@ package main.model.enums;
 import java.util.ArrayList;
 
 public enum EScomuniche {
-	RICEVI_MENO_PM(				"Receive less MP", 	"scomunica 1_1", 1), 
-	MENO_QUATTRO_ON_TERRITORIO(	"-4 on Territory", 	"scomunica 1_2", 2), 
-	NO_PV_PERSONAGGIO(			"No Charachter VP", "scomunica 1_3", 3);
+	
+	//Prima fila
+	RICEVI_MENO_PM(				   "Receive less MP", 			 "scomunica 1_1", 1), 
+	MENO_QUATTRO_ON_TERRITORIO(	   "-4 on Territory", 			 "scomunica 1_2", 2), 
+	NO_PV_PERSONAGGIO(			   "No Charachter VP", 			 "scomunica 1_3", 3),
+	
+	//;
+	//Seconda fila
+	RICEVI_MENO_MONETE(			   "Receive less C", 	 		 "scomunica 2_1", 1),
+	MENO_QUATTRO_ON_EDIFICIO(	   "-4 on Building", 	 		 "scomunica 2_2", 2),
+	NO_PV_IMPRESA(				   "No Venture VP", 	 	 	 "scomunica 2_3", 3),
+	
+	//;
+	//Terza fila
+	RICEVI_MENO_SERVITORI(		   "Receive less Sv", 	 		 "scomunica 3_1", 1),
+	MENO_QUATTRO_ON_PERSONAGGIO(   "-4 on Charachter",	 		 "scomunica 3_2", 2),
+	NO_PV_TERRITORIO(			   "No Territory VP", 	 		 "scomunica 3_3", 3),
+	
+	//;
+	//Quarta fila
+	RICEVI_MENO_LEGNA_PIETRA(	   "Receive less W, St",	 	 "scomunica 4_1", 1),
+	MENO_QUATTRO_ON_IMPRESA(	   "-4 on Venture",	 	 		 "scomunica 4_2", 2),
+	PV_MENO_CINQUE_PV(			   "VP-5 foreach VP",	 		 "scomunica 4_3", 3),
+	
+	//;
+	//Quinta fila
+	MENO_TRE_ON_RACCOLTO(		   "-3 on Harvest",	 	 		 "scomunica 5_1", 1),
+	NO_MERCATO(					   "No Market",	 	 	 		 "scomunica 5_2", 2),
+	PV_MENO_UNO_PM(				   "VP-1 foreach MP",	 		 "scomunica 5_3", 3),
+
+	//;
+	//Sesta fila
+	MENO_TRE_ON_PRODUZIONE(		   "-3 on Production",	 		 "scomunica 6_1", 1),
+	DIVISO_DUE_ON_INCREMENTO( 	   "Sv/2 on Increment", 	 	 "scomunica 6_2", 2),
+	PV_MENO_UNO_LEGNO_PIETRA_CARTE("VP-1 foreach W, St cards",	 "scomunica 6_3", 3),
+	
+	//;
+	//Settima fila
+	MENO_UNO_ON_FAMIGLARI(		   "-1 on Familiars",	 		 "scomunica 7_1", 1),
+	SALTA_PRIMA_AZIONE_TURNO( 	   "Skip the first move", 		 "scomunica 7_2", 2),
+	PV_MENO_UNO_RISERVA(	  	   "VP-1 foreach W, St, Sv, C",	 "scomunica 7_3", 3);
+
 
 	private String nome;
 	private int periodo;
