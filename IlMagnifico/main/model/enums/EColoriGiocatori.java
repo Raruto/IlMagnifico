bf08f03c10034c8d7b5f4f10889afe34aa1742bb
Raruto <Matteo@ASUS-F550L>
2017-06-18 11:01:44 +0200

    justified EColori

--- IlMagnifico/main/model/enums/EColoriGiocatori.java
+++ IlMagnifico/main/model/enums/EColoriGiocatori.java
@@ -3,7 +3,10 @@ package main.model.enums;
 import main.util.ANSI;
 
 public enum EColoriGiocatori {
-	RED(ANSI.RED, ANSI.BACKGROUND_RED, "rosso"), GREEN(ANSI.GREEN,ANSI.BACKGROUND_GREEN,"verde"), BLUE(ANSI.BLUE,ANSI.BACKGROUND_BLUE,"blu"), YELLOW(ANSI.YELLOW,ANSI.BACKGROUND_YELLOW,"giallo");
+	RED(	ANSI.RED, 	ANSI.BACKGROUND_RED, 	"rosso"), 
+	GREEN(	ANSI.GREEN,	ANSI.BACKGROUND_GREEN,	"verde"), 
+	BLUE(	ANSI.BLUE,	ANSI.BACKGROUND_BLUE,	"blu"), 
+	YELLOW(	ANSI.YELLOW,ANSI.BACKGROUND_YELLOW,	"giallo");
 
 	private String ANSICode;
 	private String ANSIBackgroundCode;
